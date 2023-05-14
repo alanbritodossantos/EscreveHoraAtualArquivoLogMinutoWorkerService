924b74a71f29342ec881649ebf3bafbb57dcd696
Alan <alanbritodossantos2015@gmail.com>
2023-05-14 12:21:49 -0300

    foi criado o metodo que registra os dados de segundo

--- EscreveHoraAtualArquivoLogMinutoWorkerService/Worker.cs
+++ EscreveHoraAtualArquivoLogMinutoWorkerService/Worker.cs
@@ -3,10 +3,12 @@ namespace EscreveHoraAtualArquivoLogMinutoWorkerService
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        int count;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
+            count= 0;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -14,8 +16,17 @@ namespace EscreveHoraAtualArquivoLogMinutoWorkerService
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                WriteCurrentTimeToLogFile();
                 await Task.Delay(1000, stoppingToken);
             }
+        }
+
+        private void WriteCurrentTimeToLogFile()
+        {
+            string logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log.txt");
+            using StreamWriter logFileWriter = new StreamWriter(logFilePath, true);
+            logFileWriter.WriteLine($"{count} - Current Time: {DateTimeOffset.Now}");
+            count++;
         }
     }
 }
