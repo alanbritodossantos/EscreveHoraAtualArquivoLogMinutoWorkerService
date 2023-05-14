6e459f961d89b5cd50d0f63b4442021de55ed9e0
Alan <alanbritodossantos2015@gmail.com>
2023-05-14 11:08:24 -0300

    Add project files.

---
+++ EscreveHoraAtualArquivoLogMinutoWorkerService/Program.cs
@@ -0,0 +1,17 @@
+namespace EscreveHoraAtualArquivoLogMinutoWorkerService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
